9835ea22ac795717ed9ec4e296d364780c53c821
RodrigoFantechi <rodrifante595@libero.it>
2022-10-21 12:12:37 +0200

    finish exercise

---
+++ snack2/main.js
@@ -0,0 +1,15 @@
+/* A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+
+const parole = ["pippo", "PLUTO","Paperino"];
+
+
+const secondoArrayConLettereInizialiMaiuscole = parole.map(element => {
+  let newWord = element.toLowerCase();
+  console.log(newWord);
+return  newWord.charAt(0).toUpperCase()+newWord.slice(1);
+
+});
+
+console.log(secondoArrayConLettereInizialiMaiuscole);
